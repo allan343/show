605aab9c627336fa1a34ab210a5d18a05271b26b
Allan Cheng <allanhfcheng@gmail.com>
2019-12-06 00:25:54 -0800

    startshow test

---
+++ src/StartShow/StartShow.test.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faPlus, faChevronLeft, faTrashAlt, faCheckDouble
+} from '@fortawesome/free-solid-svg-icons'
+import StartShow from './StartShow'
+
+library.add(faPlus, faChevronLeft, faTrashAlt, faCheckDouble)
+
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(
+    <BrowserRouter>
+      <StartShow />
+    </BrowserRouter>,
+    div
+  )
+  ReactDOM.unmountComponentAtNode(div)
+})
