d53723bfd3be37bdc93950ef39cf41b7ff28b2d8
Allan Cheng <allanhfcheng@gmail.com>
2019-12-06 14:14:02 -0800

    testing problems

--- src/Show/show.test.js
+++ src/Show/show.test.js
@@ -6,14 +6,33 @@ import {
   faPlus, faChevronLeft, faTrashAlt, faCheckDouble
 } from '@fortawesome/free-solid-svg-icons'
 import Show from './Show';
+import ApiContext from '../ApiContext/ApiContext'
+import App from '../App/App'
 
 library.add(faPlus, faChevronLeft, faTrashAlt, faCheckDouble)
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
+
+  let myShow= {
+    showname:"showname",
+    id: 2,
+  watching:false,
+  towatch:false,
+  watchLogVisible:false
+  }
+
+  let app = <App/>
+  app.context ={
+    getShow: myShow
+
+  }
+
   ReactDOM.render(
-    <BrowserRouter>
-{//<Show showId={3000} showname ={'bob'}/>
+
+  <BrowserRouter>
+  
+{<Show id ={2} />
 }
     </BrowserRouter>,
     div
