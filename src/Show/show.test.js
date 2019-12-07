e9b43a817fdc3d29deda28c32095eee09118f8e1
Allan Cheng <allanhfcheng@gmail.com>
2019-12-06 23:14:27 -0800

    stuff

--- src/Show/show.test.js
+++ src/Show/show.test.js
@@ -5,34 +5,16 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import {
   faPlus, faChevronLeft, faTrashAlt, faCheckDouble
 } from '@fortawesome/free-solid-svg-icons'
-import Show from './Show';
-import ApiContext from '../ApiContext/ApiContext'
-import App from '../App/App'
+import Show from './Show'
 
 library.add(faPlus, faChevronLeft, faTrashAlt, faCheckDouble)
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-
-  let myShow= {
-    showname:"showname",
-    id: 2,
-  watching:false,
-  towatch:false,
-  watchLogVisible:false
-  }
-
-  let app = <App/>
-  app.context ={
-    getShow: myShow
-
-  }
-
   ReactDOM.render(
-
-  <BrowserRouter>
-  
-{<Show id ={2} />
+    <BrowserRouter>
+    {
+    // <Show />
 }
     </BrowserRouter>,
     div
